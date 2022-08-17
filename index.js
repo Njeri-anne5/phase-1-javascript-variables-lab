11cc1b7d1eaad9f88798e5ae33f8321678656c20
+ <+>
2022-08-17 05:39:54 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+const companyName="Scuber";
+
+let mostProfitableNeighborhood="Chelsea"
+/*function mostProfitableNeighborhood(){
+
+}
+mostProfitableNeighborhood();*/
+
+
+let companyCeo="Susan Smith";
+/*function companyCeo(){
+
+}
+companyCeo();*/
